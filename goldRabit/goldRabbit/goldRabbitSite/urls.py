0109461bca07d8fbf66f7ff57fef38058cf41b1c
Mint-Candy95 <hakkaame626@gmail.com>
2022-07-15 20:22:12 +0900

    예약 페이지 기능 추가 구현

--- goldRabit/goldRabbit/goldRabbitSite/urls.py
+++ goldRabit/goldRabbit/goldRabbitSite/urls.py
@@ -6,10 +6,15 @@ app_name = 'goldRabbiSite'
 urlpatterns = [
     path('', views.goldrabbit_main, name='main'),
     path('reservate/',views.goldrabbit_reservate, name="reservate"),
+    path('reservate_noti/',views.goldrabbit_reservate_noti, name="reservate_noti"),
     path('notify/', views.goldrabbit_notification, name="notify"),
     path('notify/<int:noti_num>', views.goldrabbit_notificationDetail, name="notify"),
     path('myreserved/', views.goldrabbit_myreserved, name="myreserved"),
+    path('myreservedResult/', views.goldrabbit_myreservedResult, name="myreservedResult"),
     path('contact/', views.goldrabbit_contact, name="contact"),
     path('howto/', views.goldrabbit_howto, name="howto"),
-    path('album/', views.goldrabbit_album, name="album")
+    path('album/', views.goldrabbit_album, name="album"),
+    path('admin/', views.goldrabbit_admin, name="admin"),
+    path('adminLogin/', views.goldrabbit_adminLogin, name="adminLogin"),
+    
 ]
--- goldRabit/goldRabbit/goldRabbitSite/views.py
+++ goldRabit/goldRabbit/goldRabbitSite/views.py
@@ -21,11 +21,23 @@ def goldrabbit_main(request) :
     return render(request, 'goldRabbitSite/goldrabbit_main.html', context)
 
 def goldrabbit_reservate(request) :
-    
+    init_date = request.POST.get('select_date')
+    print(request.POST)
     context = {
-        
+        'init_date' : init_date
     }
     return render(request, 'goldRabbitSite/reservate.html', context)
+
+def goldrabbit_reservate_noti(request) :
+    if request.method == 'POST' :
+        ##todo
+        pass
+        
+
+    context = {
+
+    }
+    return render(request, 'goldRabbitSite/reservate_noti.html', context)
 
 def goldrabbit_notification(request) :
     page = request.GET.get('page', '1')
@@ -47,11 +59,24 @@ def goldrabbit_notificationDetail(request,noti_num) :
     return render(request, 'goldRabbitSite/notification_detail.html', context)
 
 def goldrabbit_myreserved(request) :
-    
+
     context = {
-        
+
     }
     return render(request, 'goldRabbitSite/myReserve.html', context)
+
+def goldrabbit_myreservedResult(request) :
+    reserv_list = None
+    if request.method == 'POST' :
+        nick = request.POST.get('nickname')
+        pwd = request.POST.get('pwd')
+        
+        reserv_list = Reserved.objects.all().filter(reserved_name=nick, reserved_pwd = pwd).values()
+        print(reserv_list)
+    context = {
+        'reserv_list' : reserv_list
+    }
+    return render(request, 'goldRabbitSite/myReserveResult.html', context)
 
 def goldrabbit_contact(request) :
     
@@ -73,3 +98,32 @@ def goldrabbit_album(request) :
         
     }
     return render(request, 'goldRabbitSite/album.html', context)
+
+def goldrabbit_admin(request) :
+    if request=='GET' :
+        
+        return render(request, 'goldRabbitSite/adminLogin.html')
+    if request.POST.get('pwd') != 'lovey3618' :
+        context = {
+            'msg' : '비밀번호가 일치하지 않습니다냥.'
+        } 
+        return render(request, 'goldRabbitSite/adminLogin.html', context)
+    
+    reservedList = list(Reserved.objects.all().values())
+    reservJson = json.dumps(reservedList)
+    
+    notiList = list(Notification.objects.all().values())
+    notiJson = json.dumps(notiList)
+    context = {
+        'reservedList' : reservJson,
+        'notiList' : notiJson,
+        'recent_noti' : Notification.objects.order_by('-noti_date')[:1].values()
+    }
+    return render(request, 'goldRabbitSite/admin.html', context)
+
+def goldrabbit_adminLogin(request) :
+    
+    context = {
+        
+    }
+    return render(request, 'goldRabbitSite/adminLogin.html', context)
